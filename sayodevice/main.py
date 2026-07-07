0c913466b2eacc560a106351dba7927f5e149b42
bigBseat <basitniazi1231@gmail.com>
2026-07-06 20:06:47 -0700

    Add files via upload

---
+++ sayodevice/main.py
@@ -0,0 +1,18 @@
+print("Hello, World!")
+x = 5
+y = 10
+print("The sum of x and y is:", x + y)
+if x < y:
+    print("x is less than y")
+else:
+    print("x is not less than y")
+print("This is a simple Python program.")
+print("Lets do some calculations.")
+print("Try and find the value of x and y.")
+print("hello, world!")
+print("hello not hello world!")
+print("i nhate sakupen eggs")
+print("neighborhood is a good place to live")
+OverflowError
+print("I need stuff")
+print("I need stuff again")
